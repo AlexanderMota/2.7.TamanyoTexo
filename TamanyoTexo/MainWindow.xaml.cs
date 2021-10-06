34c5ea9d84ec665509f2277409b98c51f971ef7e
AlexSinMas23 <ba.mota@iesdoctorbalmis.com>
2021-10-06 22:06:38 +0200

    añado icono

--- TamanyoTexo/MainWindow.xaml.cs
+++ TamanyoTexo/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TamanyoTexo
 {
@@ -20,63 +8,13 @@ namespace TamanyoTexo
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        /*private void RBoton_Checked(object sender, RoutedEventArgs e)
-        {
-            //((RadioButton)sender).Name
-
-            switch(((RadioButton)sender).Name)
-            {
-                case "RBoton1":
-                    RBoton1.IsChecked = true;
-                    this.Texto.FontSize = 36;
-                    break;
-                case "RBoton2":
-                    RBoton1.IsChecked = true;
-                    this.Texto.FontSize = 48;
-                    break;
-                case "RBoton3":
-                    RBoton1.IsChecked = true;
-                    this.Texto.FontSize = 72;
-                    break;
-                default:
-                    break;
-
-            }
-        }
-
-        private void RBoton_Unchecked(object sender, RoutedEventArgs e)
-        {
-            switch (((RadioButton)sender).Name)
-            {
-                case "RBoton1":
-                    RBoton1.IsChecked = true;
-                    this.Texto.FontSize = 36;
-                    break;
-                case "RBoton2":
-                    RBoton1.IsChecked = true;
-                    this.Texto.FontSize = 48;
-                    break;
-                case "RBoton3":
-                    RBoton1.IsChecked = true;
-                    this.Texto.FontSize = 72;
-                    break;
-                default:
-                    break;
-
-            }
-        }*/
-
+        public MainWindow() => InitializeComponent();
         private void RBoton_Click(object sender, RoutedEventArgs e)
         {
             switch (((RadioButton)sender).Name)
             {
                 case "RBoton1":
-                    if (!(bool)RBoton1.IsChecked.Value)
+                    if (!RBoton1.IsChecked.Value)
                     {
                         RBoton1.IsChecked = false;
                         this.Texto.FontSize = 12;
@@ -88,7 +26,7 @@ namespace TamanyoTexo
                     }
                     break;
                 case "RBoton2":
-                    if (!(bool)RBoton2.IsChecked.Value)
+                    if (!RBoton2.IsChecked.Value)
                     {
                         RBoton2.IsChecked = false;
                         this.Texto.FontSize = 12;
@@ -100,7 +38,7 @@ namespace TamanyoTexo
                     }
                     break;
                 case "RBoton3":
-                    if (!(bool)RBoton3.IsChecked.Value)
+                    if (!RBoton3.IsChecked.Value)
                     {
                         RBoton3.IsChecked = false;
                         this.Texto.FontSize = 12;
@@ -111,9 +49,7 @@ namespace TamanyoTexo
                         this.Texto.FontSize = 72;
                     }
                     break;
-                default:
-                    break;
-
+                default: break;
             }
         }
     }
